3594040bf8bd363ff51fc312f2cbdfeed4ee4bb3
fragarach63 <88527788+fragarach63@users.noreply.github.com>
2021-08-06 16:27:42 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,8 +23,6 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == '我好帥喔':
-        #刪除傳送者的訊息
-        await message.delete()
         #然後回傳訊息
         await message.channel.send('不好意思，不要騙人啦')
 
