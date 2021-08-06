eb250144d4b6099a6a7f77103d9d20746f79f212
fragarach63 <88527788+fragarach63@users.noreply.github.com>
2021-08-06 16:09:43 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,38 +1,23 @@
+#導入 Discord.py
 import discord
-import googletrans
-import os
-from pprint import pprint
-# 輸入自己Bot的TOKEN碼
-TOKEN = os.environ['TOKEN']
-SRCLanguage=os.environ['SRC']
-DSTLanguage=os.environ['DST']
-
+#client 是我們與 Discord 連結的橋樑
 client = discord.Client()
 
-# 起動時呼叫
+#調用 event 函式庫
 @client.event
+#當機器人完成啟動時
 async def on_ready():
-    print('成功登入')
+    print(f'目前登入身份：{client.user}')
 
-# 收到訊息時呼叫
+#調用 event 函式庫
 @client.event
+#當有訊息時
 async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
         return
-    
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
+    #如果我們說了「嗨」，機器人就會跟我們說「你好呀」
+    if message.content == '嗨':
+        await message.channel.send('你好呀')
 
-# Bot起動
-client.run(TOKEN)
+client.run('你的機器人 TOKEN') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
