428dbb15ae9bc7371d81253417102ab6289325df
fragarach63 <88527788+fragarach63@users.noreply.github.com>
2021-08-06 19:03:36 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -63,6 +63,17 @@ async def on_message(message):
         await message.channel.send("你要我說什麼啦？")
       else:
         await message.channel.send(tmp[1])
+        
+    if message.content.startswith('更改狀態'):
+        #切兩刀訊息
+        tmp = message.content.split(" ",2)
+        #如果分割後串列長度只有1
+        if len(tmp) == 1:
+            await message.channel.send("你要改成什麼啦？")
+        else:
+            game = discord.Game(tmp[1])
+            #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+            await client.change_presence(status=discord.Status.idle, activity=game)
 
     elif client.user in message.mentions: # @判定
         translator = googletrans.Translator()
